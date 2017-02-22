7b5c502a258526f679fc51bd3b1dfbbee83bd705
thomasmodeneis <thomas.modeneis@gmail.com>
2017-02-22 18:25:03 +0100

    #3 - removing old code, fixing index.jade

--- js/schema.js
+++ js/schema.js
@@ -1,8 +1,3 @@
-// const thinky = require('../js/thinky');
-
-// const r = thinky.r;
-// const type = thinky.type;
-
 var Schema = function(thinky){
     const Items = thinky.createModel('items', {
         id: thinky.type.string(),
@@ -31,73 +26,8 @@ var Schema = function(thinky){
         User: User
     };
     return schema;
-}
-
-
-// Relations
-
-// Items.belongsTo(User, 'user', 'userId', 'id');
-//User.hasMany(Items, 'items', 'id', 'userId');
+};
 
 module.exports = function(thinky){
     return new Schema(thinky)
 };
-
-// const Post = thinky.createModel('posts', {
-//   id          : type.string(),
-//   title       : type.string().required(),
-//   image       : type.string().required(),
-//   imageHeight : type.string(),
-//   imageWidth  : type.string(),
-//   createdAt   : type.date().default(r.now()),
-//   authorId    : type.string().required()
-// });
-// Post.ensureIndex('createdAt');
-//
-// const Usery = thinky.createModel('users', {
-//   username  : type.string().required(),
-//   password  : type.string().required(),
-//   salt      : type.string().required(),
-//   createdAt : type.date().default(r.now())
-// }, {
-//   pk : 'username'
-// });
-
-// const Comment = thinky.createModel('comments', {
-//   id        : type.string(),
-//   content   : type.string().required(),
-//   createdAt : type.date().default(r.now()),
-//   authorId  : type.string().required(),
-//   postId    : type.string().required()
-// });
-// Comment.ensureIndex('createdAt');
-
-// const Likey = thinky.createModel('likes', {
-//   id        : type.string(),
-//   createdAt : type.date().default(r.now()),
-//   authorId  : type.string().required(),
-//   postId    : type.string().required()
-// });
-
-// Relations
-
-// Post.belongsTo(User, 'author', 'authorId', 'username');
-// Post.hasMany(Comment, 'comments', 'id', 'postId');
-// Post.hasMany(Like, 'likes', 'id', 'postId');
-//
-// User.hasMany(Post, 'posts', 'id', 'authorId');
-// User.hasMany(Comment, 'comments', 'id', 'authorId');
-// User.hasMany(Like, 'likes', 'id', 'authorId');
-//
-// Comment.belongsTo(User, 'author', 'authorId', 'username');
-// Comment.belongsTo(Post, 'post', 'postId', 'id');
-//
-// Like.belongsTo(User, 'author', 'authorId', 'username');
-// Like.belongsTo(Post, 'post', 'postId', 'id');
-//
-// module.exports = {
-//   Post,
-//   User,
-//   Comment,
-//   Like
-// };
