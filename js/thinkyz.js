d52b84c28fcdd7cefa61a1e4c2df0431224a4d2c
thomasmodeneis <thomas.modeneis@gmail.com>
2017-02-22 18:18:20 +0100

    #3 - removing un-used classes

--- js/thinkyz.js
+++
@@ -1,9 +0,0 @@
-const thinky = require('thinky')({
-  host     : process.env.RETHINKDB_HOST || 'g7-box',
-  port     : process.env.RETHINKDB_PORT || 28015,
-  db       : process.env.RETHINKDB_DB || 'hapiger_it',
-  user     : process.env.RETHINKDB_USER,
-  password : process.env.RETHINKDB_PASSWORD
-});
-
-module.exports = thinky;
